ec4dd2a5cc228e048d28b38c58579a49f504ce85
alexlhanson <alexlhanson@gmail.com>
2018-07-26 10:23:56 -0700

    Add main nav hide/show

--- lab/alex-tim/scripts/articleView.js
+++ lab/alex-tim/scripts/articleView.js
@@ -74,10 +74,19 @@ articleView.handleCategoryFilter = function() {
 };
 
 articleView.handleMainNav = function() {
-  // TODO: Add an event handler to nav elements that will power the Tabs feature.
+  // DONE: Add an event handler to nav elements that will power the Tabs feature.
   // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
   // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
-  
+  $('nav .tab:first').on('click',function(){
+    $('.tab-content').fadeIn();
+    $('#about').hide();
+  })
+
+  $('nav .tab:first ~ li').on('click',function(){
+    $('.tab-content').hide();
+    $('#about').fadeIn();
+
+  })
   // REVIEW: Now trigger a click on the first .tab element, to set up the page.
   $('nav .tab:first').click();
 };
@@ -95,4 +104,6 @@ $(document).ready(function() {
   articleView.populateFilters();
   articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
+  articleView.handleMainNav();
+  articleView.setTeasers();
 })
